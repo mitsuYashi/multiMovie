81253c76e5bd5605d6405a2515022516fa6185dc
mitsuYashi <hm20011027@gmail.com>
2022-03-16 17:48:49 +0900

    add: mapのkey

--- app/components/ListMovie.tsx
+++ app/components/ListMovie.tsx
@@ -6,8 +6,8 @@ const ListMovie: React.FC<Props> = ({ movieIds }) => {
 
   return (
     <div style={{width: "10vw"}}>
-      {movieIds.slice(4,).map((id) => (
-          <p>{id}</p>
+      {movieIds.slice(4,).map((id, index) => (
+          <p key={index}>{id}</p>
       ))}
     </div>
   );
