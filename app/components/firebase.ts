8e1b5a3c23dc7d6abcbbc52fc5f38e252aebc771
mitsuYashi <hm20011027@gmail.com>
2022-03-16 11:26:22 +0900

    add: firebase config

---
+++ app/components/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCavPBkH9wuFdZrpyT3tgSDJ7J82AAy_P0",
+  authDomain: "multimovie-ca626.firebaseapp.com",
+  projectId: "multimovie-ca626",
+  storageBucket: "multimovie-ca626.appspot.com",
+  messagingSenderId: "513035858222",
+  appId: "1:513035858222:web:41f5223c94e83e0724cabf"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
