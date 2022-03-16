f11d733bdbcd9ac1c72870b1e7bba1c08f6474c4
mitsuYashi <hm20011027@gmail.com>
2022-03-16 17:54:29 +0900

    add: キューから呼び出し

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,48 +1,79 @@
+import { css } from "@emotion/react";
 import { NextPage } from "next";
 import React, { useEffect, useState } from "react";
+import ButtonCross from "/components/ButtonCross";
 import InputUrl from "/components/InputUrl";
 import ListMovie from "/components/ListMovie";
 import Movie from "/components/Movie";
 
-const Index: NextPage = () => {
-  const [movieIds, setMovieIds] = useState<string[]>([]);
-  const [displayMovieId, setDisplayMovieId] = useState<string[]>([
-    "",
-    "",
-    "",
-    "",
-  ]);
+const classes = {
+  movies: css`
+    height: "99vh";
+    width: 88vw;
+    display: grid;
+    grid-template-columns: 44.5vw 44.5vw;
+    grid-template-rows: 49vh 49vh;
+  `,
+  flex: css`
+    display: flex;
+    max-width: 10vw;
+  `,
+};
 
-  useEffect(() => {
-    
-  }, []);
+const Index: NextPage = () => {
+  const [movieIds, setMovieIds] = useState<string[]>(["", "", "", ""]);
+
+  useEffect(() => {}, []);
 
   const addMovieId = (url: string) => {
-    setMovieIds([...movieIds, url]);
-
-    // setDisplayMovieId(
-    //   displayMovieId.map((movieId) => (movieId == "" ? url : ""))
-    // );
+    let flag = true;
+    const newMovieIds = movieIds.map((str, index) => {
+      if (str == "" && flag == true) {
+        flag = false;
+        return url;
+      } else {
+        return str;
+      }
+    });
+    setMovieIds(newMovieIds);
+    if (flag) {
+      setMovieIds([...movieIds, url]);
+    }
 
     console.log(url);
   };
 
+  const updateMovieId = (index: number) => {
+    let newMovieIds = movieIds.map((str, ind) => {
+      if (ind == index) {
+        return movieIds[4] ?? "";
+      } else {
+        return str;
+      }
+    });
+
+    newMovieIds.splice(4, 1);
+
+    setMovieIds([...newMovieIds]);
+  };
+
   return (
     <div>
-      <InputUrl addMovieId={addMovieId} />
-      <div style={{display: "flex"}}>
-        <ListMovie movieIds={movieIds} />
-        <div
-          style={{
-            height: "90vh",
-            width: "100vw",
-            display: "grid",
-            gridTemplateColumns: "45vw 45vw",
-            gridTemplateRows: "45vh 45vh",
-          }}
-        >
+      {console.log(movieIds)}
+      <div css={classes.flex}>
+        <div>
+          <InputUrl addMovieId={addMovieId} />
+          <ListMovie movieIds={movieIds} />
+        </div>
+        <div css={classes.movies}>
           {movieIds.slice(0, 4).map((movieId, index) => (
-            <Movie movieId={movieId} key={index} />
+            <Movie movieId={movieId} key={index}>
+              <ButtonCross
+                key={index}
+                index={index}
+                updateMovieId={updateMovieId}
+              />
+            </Movie>
           ))}
         </div>
       </div>
