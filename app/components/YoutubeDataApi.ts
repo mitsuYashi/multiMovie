64e949a711f20cc9fc1ecf16b7b029d833971d69
mitsuYashi <hm20011027@gmail.com>
2022-04-05 19:54:41 +0900

    add: get youtubeDataApi

---
+++ app/components/YoutubeDataApi.ts
@@ -0,0 +1,15 @@
+import axios from "axios";
+import useSWR from "swr";
+
+const YoutubeDataApi = async (movieId: string) => {
+  const endpoint = "https://www.googleapis.com/youtube/v3/videos";
+  const req = `${endpoint}?part=snippet&id=${movieId}&key=AIzaSyCavPBkH9wuFdZrpyT3tgSDJ7J82AAy_P0`;
+  try {
+    const res = await axios.get(req);
+    return res.data.items[0].snippet.title;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export default YoutubeDataApi;
