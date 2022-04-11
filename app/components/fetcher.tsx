f1fb7e3648e9398948072e3056b7b35aeb1bd794
mitsuYashi <hm20011027@gmail.com>
2022-04-11 20:41:16 +0900

    add: 履歴用fetcher作成

--- app/components/fetcher.tsx
+++ app/components/fetcher.tsx
@@ -1,16 +1,48 @@
 import axios from "axios";
 import useSWR from "swr";
-import { listenAuthState } from "./firebase";
-import { User } from "/type";
+import { listenAuthState, userToken } from "./firebase";
+import { ServerMovies, user } from "/type";
 
-export const fetcher = (url: string): Promise<User> => {
-  return listenAuthState()
-    .then((token) => {
-      return axios.get(`${process.env.originAPI}/${url}`, {
+export const getUserFetcher = async (url: string): Promise<user> => {
+  return axios
+    .get(`${process.env.originAPI}/${url}`, {
+      headers: {
+        Authorization: `Bearer ${await userToken()}`,
+      },
+    })
+    .then((res) => res.data);
+};
+
+export const getMovieFetcher = async (url: string): Promise<ServerMovies> => {
+  return axios
+    .get(`${process.env.originAPI}/${url}`, {
+      headers: {
+        Authorization: `Bearer ${await userToken()}`,
+      },
+    })
+    .then((res) => res.data);
+};
+
+export const putPlayListFetcher = async (url: string): Promise<void> => {
+  return axios
+    .put(
+      `${process.env.originAPI}/${url}`,
+      {},
+      {
         headers: {
-          Authorization: `Bearer ${token}`,
+          Authorization: `Bearer ${await userToken()}`,
         },
-      });
+      }
+    )
+    .then((res) => res.data);
+};
+
+export const postFetcher = async (url: string, payload: Object) => {
+  return axios
+    .post(`${process.env.originAPI}/${url}`, payload, {
+      headers: {
+        Authorization: `Bearer ${await userToken()}`,
+      },
     })
     .then((res) => res.data);
 };
