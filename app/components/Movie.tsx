69f6626b77e4c1cbd544d67db99d88517dd14d18
mitsuYashi <hm20011027@gmail.com>
2022-04-11 20:39:32 +0900

    change: dummy画面移動

--- app/components/Movie.tsx
+++ app/components/Movie.tsx
@@ -19,14 +19,6 @@ const classes = {
     height: 49vh;
     width: 44.5vw;
   `,
-  noMovie: css`
-    height: 49vh;
-    width: 44.5vw;
-    background-color: #fafdff;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  `,
 };
 
 const Movie: React.FC<Props> = ({
@@ -38,31 +30,22 @@ const Movie: React.FC<Props> = ({
   // onReady,
 }) => {
   return (
-    <>
-      {movieId != "" ? (
-        <div css={classes.wrap}>
-          {children}
-          <YouTube
-            videoId={movieId}
-            onEnd={() =>
-              isAutoPlayEnd["end"] == 1 ? updateMovieId(index) : null
-            }
-            css={classes.movie}
-            opts={{ playerVars: { autoplay: isAutoPlayEnd["autoplay"] } }}
-            // onReady={onReady}
-          />
-          {/* <iframe
+    <div css={classes.wrap}>
+      {children}
+      <YouTube
+        videoId={movieId}
+        onEnd={() => (isAutoPlayEnd["end"] == 1 ? updateMovieId(index) : null)}
+        css={classes.movie}
+        opts={{ playerVars: { autoplay: isAutoPlayEnd["autoplay"] } }}
+      />
+      {/* <iframe
             src={`https://www.youtube.com/embed/${movieId}?enablejsapi=1&autoplay=1`}
             title="YouTube video player"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
           ></iframe> */}
-        </div>
-      ) : (
-        <div css={classes.noMovie}>動画を登録してください。</div>
-      )}
-    </>
+    </div>
   );
 };
 
