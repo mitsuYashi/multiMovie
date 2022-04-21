5d90cd96d055eb8dfa021b41e5bd236ed174f14a
mitsuYashi <hm20011027@gmail.com>
2022-04-21 12:36:05 +0900

    add: throw Error

--- app/components/fetcher.tsx
+++ app/components/fetcher.tsx
@@ -1,50 +1,74 @@
 import axios from "axios";
 import useSWR from "swr";
 import { listenAuthState, userToken } from "./firebase";
-import { ServerMovies, user } from "/type";
+import { ServerMovies, User } from "/type";
 
-export const getUserFetcher = async (url: string): Promise<user> => {
-  return axios
-    .get(`${process.env.originAPI}/${url}`, {
-      headers: {
-        Authorization: `Bearer ${await userToken()}`,
-      },
-    })
-    .then((res) => res.data);
+export const getUserFetcher = async (url: string): Promise<User> => {
+  const token = await userToken();
+  if (token == undefined) {
+    const error = new Error("cannot find Account token");
+    throw error;
+  } else {
+    return axios
+      .get(`${process.env.originAPI}/${url}`, {
+        headers: {
+          Authorization: `Bearer ${await token}`,
+        },
+      })
+      .then((res) => res.data);
+  }
 };
 
 export const getMovieFetcher = async (url: string): Promise<ServerMovies> => {
-  return axios
-    .get(`${process.env.originAPI}/${url}`, {
-      headers: {
-        Authorization: `Bearer ${await userToken()}`,
-      },
-    })
-    .then((res) => res.data);
+  const token = await userToken();
+  if (token == undefined) {
+    const error = new Error("cannot find Account token");
+    throw error;
+  } else {
+    return axios
+      .get(`${process.env.originAPI}/${url}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => res.data);
+  }
 };
 
 export const putPlayListFetcher = async (url: string): Promise<void> => {
-  return axios
-    .put(
-      `${process.env.originAPI}/${url}`,
-      {},
-      {
-        headers: {
-          Authorization: `Bearer ${await userToken()}`,
-        },
-      }
-    )
-    .then((res) => res.data);
+  const token = await userToken();
+  if (token == undefined) {
+    const error = new Error("cannot find Account token");
+    throw error;
+  } else {
+    return axios
+      .put(
+        `${process.env.originAPI}/${url}`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${await token}`,
+          },
+        }
+      )
+      .then((res) => res.data);
+  }
 };
 
 export const postFetcher = async (url: string, payload: Object) => {
-  return axios
-    .post(`${process.env.originAPI}/${url}`, payload, {
-      headers: {
-        Authorization: `Bearer ${await userToken()}`,
-      },
-    })
-    .then((res) => res.data);
+  const token = await userToken();
+  if (token == undefined) {
+    const error = new Error("cannot find Account token");
+    throw error;
+  } else {
+    return axios
+      .post(`${process.env.originAPI}/${url}`, payload, {
+        headers: {
+          Authorization: `Bearer ${await userToken()}`,
+        },
+      })
+      .then((res) => res.data);
+  }
 };
 
 export const noLoginFetcher = (url: string) =>
